b413e8f52bc881eae4ba0a300c3d883298af5d54
shenglin-alex <a182820998@gmail.com>
2019-12-25 17:48:37 +0800

    add logout task

--- locustfile.py
+++ locustfile.py
@@ -2,26 +2,46 @@ from locust import HttpLocust, TaskSet, task, between
 
 
 class WebsiteTasks(TaskSet):
+    xsrf_token = ''
     cookieJar = {}
 
     def on_start(self):
-        headers = {"content-type": "multipart/form-data"}
+        with self.client.get("/moe/public/login/",
+                             catch_response=True) as response:
+            self.cookieJar = response.cookies
+            self.xsrf_token = response.cookies["XSRF-TOKEN"]
+
+    @task(1)
+    def login(self):
+        headers = {"content-type": "multipart/form-data",
+                   "X-XSRF-TOKEN": self.xsrf_token}
         payload = {"account": "admin", "password": "123456"}
+
+        # login
         with self.client.post("/moe/public/eFound/login",
                               catch_response=True,
                               data=payload,
-                              headers=headers) as response:
+                              headers=headers,
+                              cookies=self.cookieJar) as response:
             self.cookieJar = response.cookies
 
-    @task(1)
-    def get_external_announce(self):
-        with self.client.get("/moe/public/eFound/managMoe/externalAnnounce/",
-                             catch_response=True,
-                             cookies=self.cookieJar) as response:
-            if response.status_code == 302:
-                response.failure("Auth not pass!")
-            elif response.status_code == 200:
-                response.success()
+        # logout
+        headers = {"X-XSRF-TOKEN": self.xsrf_token}
+        with self.client.delete("/moe/public/logout",
+                                catch_response=True,
+                                headers=headers,
+                                cookies=self.cookieJar) as response:
+            self.cookieJar = response.cookies
+
+    # @task(2)
+    # def get_external_announce(self):
+    #     with self.client.get("/moe/public/eFound/managMoe/externalAnnounce/",
+    #                          catch_response=True,
+    #                          cookies=self.cookieJar) as response:
+    #         if response.status_code != 200:
+    #             response.failure("Auth not pass!")
+    #         else:
+    #             response.success()
 
 
 class WebsiteUser(HttpLocust):
